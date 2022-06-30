a593a17cfc349796186979351c26bda63bdf2ddb
Guusvanmeerveld <Guusvanmeerveld@users.noreply.github.com>
2022-06-30 15:30:18 +0000

    deploy: 2ef296e720d463a0286d8ad22df76d355a86c18a

---
+++ assets/splashscreen.75f33778.js
@@ -0,0 +1,2 @@
+import{S as p,e}from"./700.6bfd073f.js";const t=()=>e("div",{children:"splashy"});p(e(t,{}),document.getElementById("app"));
+//# sourceMappingURL=splashscreen.75f33778.js.map
--- assets/splashscreen.b6613c73.js
+++
@@ -1,2 +0,0 @@
-import{S as p,j as e}from"./700.f5fd593a.js";const t=()=>e("div",{children:"splashy"});p(e(t,{}),document.getElementById("app"));
-//# sourceMappingURL=splashscreen.b6613c73.js.map
