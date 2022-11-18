dc556ba31372874f5638431735b31ec13ce9d9b4
Guusvanmeerveld <mail@guusvanmeerveld.dev>
2022-11-18 02:05:16 +0100

    chore(server): create check to test if provided hostname is actually a hostname
    this prevents xss attacks

--- apps/server/src/utils/createOAuthPage.ts
+++ apps/server/src/utils/createOAuthPage.ts
@@ -5,8 +5,12 @@ import { join } from "path";
 
 import createTokenResponse from "./createTokenResponse";
 
+import { BadRequestException } from "@nestjs/common";
+
 const pathToOAuthPage = join(process.cwd(), "public", "oauth.html");
 const pathToOAuthScript = join(process.cwd(), "public", "oauth.js");
+
+const hostnameMatch = /\w+:\/\/\w+(:[0-9]+)?/g;
 
 const createOAuthPage = async (
 	accessToken: string,
@@ -14,6 +18,9 @@ const createOAuthPage = async (
 	username: string,
 	clientHostname: string
 ): Promise<[string, string]> => {
+	if (!hostnameMatch.test(clientHostname))
+		throw new BadRequestException("Invalid hostname provided");
+
 	const tokens = {
 		access: createTokenResponse("access", accessToken),
 		refresh: createTokenResponse("refresh", refreshToken)
