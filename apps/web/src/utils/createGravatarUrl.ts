529b374cf4d41cdbcab508471c070a4656bc4c86
Guusvanmeerveld <mail@guusvanmeerveld.dev>
2022-11-21 12:00:47 +0100

    fix(web): remove file that is not used anymore

--- apps/web/src/utils/createGravatarUrl.ts
+++
@@ -1,8 +0,0 @@
-import md5 from "js-md5";
-
-const createGravatarUrl = (email: string): string =>
-	`https://www.gravatar.com/avatar/${md5(
-		email.trim().toLocaleLowerCase()
-	)}?d=404`;
-
-export default createGravatarUrl;
